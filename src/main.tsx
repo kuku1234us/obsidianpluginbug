5ab0ea42ad77c5767c75727e3b37f858e0517c55
kothreat <kuku1234us@yahoo.com>
2024-11-14 11:55:22 +0800

    Bug Issue with iPhone Obsidian Plugin

---
+++ src/main.tsx
@@ -0,0 +1,60 @@
+import { Plugin, Modal } from "obsidian";
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// Define a custom modal class that uses React
+class PopupDialog extends Modal {
+  private root: ReactDOM.Root | null = null;
+
+  constructor(app: any, onClose: () => void) {
+    super(app);
+    this.onClose = onClose;
+  }
+
+  onClose: () => void;
+
+  onOpen() {
+    const { contentEl } = this;
+    this.modalEl.classList.add("bg-blue-500");
+
+    // Create a React root and render the HelloWorldPopup component
+    // This line works on Desktop, but fails on iPhone:
+    this.root = ReactDOM.createRoot(contentEl);
+    this.root.render(<div>Hello World</div>);
+  }
+
+  onCloseModal() {
+    // Unmount the React component when closing the modal
+    if (this.root) {
+      this.root.unmount();
+    }
+
+    // Call the onClose callback passed in the constructor
+    this.onClose();
+  }
+}
+
+// Main plugin class
+export default class MyPlugin extends Plugin {
+  async onload() {
+    console.log("Loading MyPlugin");
+
+    // Command to show the Hello World popup
+    this.addCommand({
+      id: "TestHello",
+      name: "TestHello",
+      callback: () => this.showHelloWorldPopup(),
+    });
+  }
+
+  onunload() {
+    console.log("Unloading MyPlugin");
+  }
+
+  showHelloWorldPopup() {
+    const dialog = new PopupDialog(this.app, () => {
+      console.log("Popup closed");
+    });
+    dialog.open();
+  }
+}
